0a596de86d18c0dbcad7f9e2fd0ed1fece1729ab
LooseLi <1329307562@qq.com>
2022-11-30 18:22:00 +0800

    feat(react): 组件状态的定义使用修改

--- src/App.js
+++ src/App.js
@@ -4,25 +4,44 @@ import React from "react";
 class TestComponent extends React.Component {
   // 1. 定义组件状态
   state = {
-    name: '测试一下'
+    name: '测试一下',
+    list: [1,2,3,4],
+    person: {
+      name: 'jack',
+      age: 20
+    }
   }
   // 3. 修改state中的状态name
   // 注意：不可以直接做赋值修改 必须通过一个方法 setState
   changeName = () => {
     this.setState({
       // 在这里可以定义各种属性 全都是当前组件的状态
-      name: 'loose.li'
+      name: 'loose.li',
+      list: [...this.state.list, 5],
+      person: {
+        ...this.state.person,
+        name: 'rose'
+      },
+    })
+  }
+  removeList = () => {
+    this.setState({
+      list: this.state.list.filter(item => item !== 2)
     })
   }
   render() {
     // render函数中的this已经被react内部做了修正，这里的this就是指向当前的组件实例对象
-
     // 2. 使用状态
     return (
       <div>
         <p>this is TestComponent</p>
         <p>当前名字：{this.state.name}</p>
+        <ul>
+          {this.state.list.map(item => <li key={item}>{item}</li>)}
+        </ul>
+        <p>{this.state.person.name}</p>
         <button onClick={this.changeName}>修改名称</button>
+        <button onClick={this.removeList}>删除数组项</button>
       </div>
     )
   }
