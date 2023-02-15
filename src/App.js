8a4183fe42df5b7abba90cd84a644f5632d17833
LooseLi <1329307562@qq.com>
2023-02-15 16:04:36 +0800

    feat(react): router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,3 @@
-import React, {useRef, useEffect} from 'react';
-// useRef 获取实例，故只存在于类组件中
-class Test extends React.Component {
-  render() {
-    return <div>this is test</div>;
-  }
-}
-
-function App() {
-  const testRef = useRef(null);
-  const h1Ref = useRef(null);
-  useEffect(() => {
-    console.log(testRef.current);
-    console.log(h1Ref.current);
-  }, []);
-  return (
-    <>
-      <Test ref={testRef} />
-      <h1 ref={h1Ref}>title</h1>
-    </>
-  );
-}
+import App from './demo-router/App';
 
 export default App;
---
+++ src/demo-router/App.js
@@ -0,0 +1,25 @@
+// 引入组件
+import Home from './Home';
+import About from './About';
+import {BrowserRouter, Link, Routes, Route} from 'react-router-dom';
+
+// 进行路由配置
+
+function App() {
+  return (
+    // 声明当前要用一个非hash模式的路由
+    <BrowserRouter>
+      {/* 指定跳转的组件 to用来配置路由地址 */}
+      <Link to="/">首页</Link>
+      <Link to="/about">关于</Link>
+      {/* 路由出口 路由对应的组件会在这里进行渲染 */}
+      <Routes>
+        {/* 指定路径和组件的对应关系 path代表路径 element代表组件 成对出现 path -> element */}
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
