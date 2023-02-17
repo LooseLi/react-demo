a0d98bf9bc71bad841241d78568dafe4d86566c4
LooseLi <1329307562@qq.com>
2023-02-17 15:24:09 +0800

    feat(react): init sass

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // 应用的全局样式文件
-import './index.css';
+// import './index.css';
+import './demo-project/index.scss';
 // 引入根组件
-import App from './App';
+// import App from './App';
+import App from './demo-project/App';
 
 // 渲染根组件App到一个id为root的dom节点上
 const root = ReactDOM.createRoot(document.getElementById('root'));
