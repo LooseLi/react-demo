b20450621a191825341282d8c93fa3102f235fc8
LooseLi <1329307562@qq.com>
2023-02-17 15:46:57 +0800

    feat(react): init antd

--- src/demo-project/App.js
+++ src/demo-project/App.js
@@ -1,11 +1,13 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Layout from './pages/Layout';
 import Login from './pages/Login';
+import {Button} from 'antd';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app">
+        <Button type="primary">Primary Button</Button>
         <Routes>
           <Route path="/" element={<Layout />}></Route>
           <Route path="/login" element={<Login />}></Route>
--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import './demo-project/index.scss';
 // 引入根组件
 // import App from './App';
 import App from './demo-project/App';
+// 导入antd样式文件
+import 'antd/dist/reset.css';
 
 // 渲染根组件App到一个id为root的dom节点上
 const root = ReactDOM.createRoot(document.getElementById('root'));
