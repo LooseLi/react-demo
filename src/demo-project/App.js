84c1e7ca8aa80dfd05e9037b85a6cbd599d32075
LooseLi <1329307562@qq.com>
2023-02-17 15:32:26 +0800

    feat(react): init router

--- src/demo-project/App.js
+++ src/demo-project/App.js
@@ -1,5 +1,18 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Layout from './pages/Layout';
+import Login from './pages/Login';
+
 function App() {
-  return <div className="app">app</div>;
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Layout />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
