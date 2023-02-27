4e3f332085a2783c3f3462d16c75c8288de3b04c
LooseLi <1329307562@qq.com>
2023-02-27 15:14:12 +0800

    feat(react): login初始化

--- src/demo-project/App.js
+++ src/demo-project/App.js
@@ -1,13 +1,11 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Layout from '@/demo-project/pages/Layout';
 import Login from '@/demo-project/pages/Login';
-import {Button} from 'antd';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app">
-        <Button type="primary">Primary Button</Button>
         <Routes>
           <Route path="/" element={<Layout />}></Route>
           <Route path="/login" element={<Login />}></Route>
--- src/demo-project/pages/Login/index.js
+++ src/demo-project/pages/Login/index.js
@@ -1,5 +1,61 @@
+import {Button, Card, Checkbox, Form, Input} from 'antd';
+import './index.scss';
+
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <p className="title">管理系统</p>
+        <Form
+          validateTrigger={['onBlur', 'onChange']}
+          initialValues={{
+            remember: true,
+          }}
+        >
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: '请输入手机号',
+              },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: '请输入正确的手机号',
+                validateTrigger: 'onBlur',
+              },
+            ]}
+          >
+            <Input placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: '请输入密码',
+              },
+              {
+                len: 6,
+                message: '请输入6位密码',
+                validateTrigger: 'onBlur',
+              },
+            ]}
+          >
+            <Input.Password placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item name="remember" valuePropName="checked">
+            <Checkbox className="login-checkbox-label">我已阅读并同意「用户协议」和「隐私条款」</Checkbox>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
 }
 
 export default Login;
