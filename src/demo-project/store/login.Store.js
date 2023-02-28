50426bb772d622df5951025e3b9168ddd606997d
LooseLi <1329307562@qq.com>
2023-02-28 14:46:11 +0800

    feat(react): token持久化

--- src/demo-project/store/login.Store.js
+++ src/demo-project/store/login.Store.js
@@ -1,13 +1,16 @@
 import {makeAutoObservable} from 'mobx';
+import {setToken, getToken} from '@/demo-project/utils/index';
 
 class LoginStore {
-  token = '';
+  token = getToken() || '';
   constructor() {
     makeAutoObservable(this);
   }
   getToken = ({username, password}) => {
     console.log(username, password);
     this.token = 'leeson';
+    // 存入localStorage
+    setToken(this.token);
   };
 }
 
