da5fcf242ec9e2aa7f5527c7de115253a7c1a0c3
LooseLi <1329307562@qq.com>
2023-03-03 17:36:03 +0800

    feat(react): 联动sider和router

--- src/demo-project/pages/Layout/index.js
+++ src/demo-project/pages/Layout/index.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import {UploadOutlined, UserOutlined, VideoCameraOutlined} from '@ant-design/icons';
 import {Layout, Menu} from 'antd';
-import {Outlet} from 'react-router-dom';
+import {Outlet, Link, useLocation} from 'react-router-dom';
 import './index.scss';
 
 const {Sider} = Layout;
 
 const LayoutPages: React.FC = () => {
+  const {pathname} = useLocation();
   return (
     <Layout>
       <Sider>
-        <Menu mode="inline" theme="dark" defaultSelectedKeys={['1']}>
-          <Menu.Item icon={<UserOutlined />} key="1">
-            数据概览
+        <Menu mode="inline" theme="dark" defaultSelectedKeys={[pathname]}>
+          <Menu.Item icon={<UserOutlined />} key="/">
+            <Link to="/">数据概览</Link>
           </Menu.Item>
-          <Menu.Item icon={<UploadOutlined />} key="2">
-            内容管理
+          <Menu.Item icon={<UploadOutlined />} key="/article">
+            <Link to="/article">内容管理</Link>
           </Menu.Item>
-          <Menu.Item icon={<VideoCameraOutlined />} key="3">
-            发布文章
+          <Menu.Item icon={<VideoCameraOutlined />} key="/publish">
+            <Link to="/publish">发布文章</Link>
           </Menu.Item>
         </Menu>
       </Sider>
-      <Layout className="layout-content">
+      <Layout className="layout-content" style={{padding: 20}}>
         <Outlet />
       </Layout>
     </Layout>
