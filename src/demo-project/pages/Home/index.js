a2e81cb8a1e865362de04a04158139dcd71a8c9c
LooseLi <1329307562@qq.com>
2023-03-07 14:18:02 +0800

    feat(react): init echarts

--- src/demo-project/pages/Home/index.js
+++ src/demo-project/pages/Home/index.js
@@ -1,5 +1,46 @@
-function Home() {
-  return <div>Home</div>;
-}
+import './index.scss';
+import {useEffect, useRef} from 'react';
+import * as echarts from 'echarts';
+/**
+ * 思路：
+ * 1. 看官方文档 把echarts加入项目
+ *    如何在react获取dom -> useRef
+ *    在什么地方获取dom节点 -> useEffect
+ * 2. 不抽离定制化的参数 先把最小的demo跑起来
+ * 3. 按照需求 哪些参数需要自定义 抽象出来
+ */
+const Home = () => {
+  const domRef = useRef();
+  const initEcharts = () => {
+    // 基于准备好的dom，初始化echarts实例
+    const myChart = echarts.init(domRef.current);
+    // 绘制图表
+    myChart.setOption({
+      title: {
+        text: 'ECharts 入门示例',
+      },
+      tooltip: {},
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子'],
+      },
+      yAxis: {},
+      series: [
+        {
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20],
+        },
+      ],
+    });
+  };
+  useEffect(() => {
+    initEcharts();
+  }, []);
+  return (
+    <>
+      <div ref={domRef} style={{width: '500px', height: '300px'}}></div>
+    </>
+  );
+};
 
 export default Home;
