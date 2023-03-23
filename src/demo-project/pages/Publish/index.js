05f312f7cc1afd93de87aea580552a144deb8513
LooseLi <1329307562@qq.com>
2023-03-23 16:10:17 +0800

    feat(react): react quill

--- src/demo-project/pages/Publish/index.js
+++ src/demo-project/pages/Publish/index.js
@@ -2,6 +2,8 @@ import {Card, Breadcrumb, Form, Button, Radio, Input, Upload, Space, Select} fro
 import {PlusOutlined} from '@ant-design/icons';
 import {Link} from 'react-router-dom';
 import './index.scss';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const {Option} = Select;
 
@@ -18,7 +20,7 @@ const Publish = () => {
           </Breadcrumb>
         }
       >
-        <Form labelCol={{span: 4}} wrapperCol={{span: 16}} initialValues={{type: 1}}>
+        <Form labelCol={{span: 4}} wrapperCol={{span: 16}} initialValues={{type: 1, content: '请输入内容'}}>
           <Form.Item label="标题" name="title" rules={[{required: true, message: '请输入文章标题'}]}>
             <Input placeholder="请输入文章标题" />
           </Form.Item>
@@ -33,7 +35,7 @@ const Publish = () => {
             </Select>
           </Form.Item>
           <Form.Item label="内容" name="content" rules={[{required: true, message: '请输入文章内容'}]}>
-            <Input placeholder="请输入文章标题" />
+            <ReactQuill theme="snow" />
           </Form.Item>
 
           <Form.Item wrapperCol={{offset: 4}}>
