3f325581d853bcfa99a26354011c9f9476fcdd52
LooseLi <1329307562@qq.com>
2023-03-24 16:33:31 +0800

    feat(react): router懒加载

--- src/demo-project/App.js
+++ src/demo-project/App.js
@@ -1,32 +1,36 @@
+import {lazy, Suspense} from 'react';
 import {Routes, Route, unstable_HistoryRouter as HistoryRouter} from 'react-router-dom';
 import {history} from '@/demo-project/utils/history';
-import LayoutPages from '@/demo-project/pages/Layout';
-import Login from '@/demo-project/pages/Login';
 import {AuthComponent} from '@/demo-project/components/AuthComponent';
-import Home from '@/demo-project/pages/Home';
-import Article from '@/demo-project/pages/Article';
-import Publish from '@/demo-project/pages/Publish';
+// 按需导入
+const Home = lazy(() => import('@/demo-project/pages/Home'));
+const Login = lazy(() => import('@/demo-project/pages/Login'));
+const Article = lazy(() => import('@/demo-project/pages/Article'));
+const Publish = lazy(() => import('@/demo-project/pages/Publish'));
+const LayoutPages = lazy(() => import('@/demo-project/pages/Layout'));
 
 function App() {
   return (
     <HistoryRouter history={history}>
       <div className="app">
-        <Routes>
-          {/* Layout需要鉴权处理 */}
-          <Route
-            path="/"
-            element={
-              <AuthComponent>
-                <LayoutPages />
-              </AuthComponent>
-            }
-          >
-            <Route index element={<Home />}></Route>
-            <Route path="article" element={<Article />}></Route>
-            <Route path="publish" element={<Publish />}></Route>
-          </Route>
-          <Route path="/login" element={<Login />}></Route>
-        </Routes>
+        <Suspense fallback={<div style={{textAlign: 'center', marginTop: 200}}>loading...</div>}>
+          <Routes>
+            {/* Layout需要鉴权处理 */}
+            <Route
+              path="/"
+              element={
+                <AuthComponent>
+                  <LayoutPages />
+                </AuthComponent>
+              }
+            >
+              <Route index element={<Home />}></Route>
+              <Route path="article" element={<Article />}></Route>
+              <Route path="publish" element={<Publish />}></Route>
+            </Route>
+            <Route path="/login" element={<Login />}></Route>
+          </Routes>
+        </Suspense>
       </div>
     </HistoryRouter>
   );
